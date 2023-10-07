8022a3279252ea0657c1ed04974a5e8ac16ec2ce
griffela <100675308+Nasteeena@users.noreply.github.com>
2023-10-07 18:42:11 +0300

    second

--- src/components/pages/Cart/Cart.tsx
+++ src/components/pages/Cart/Cart.tsx
@@ -1,20 +1,20 @@
 import Header from "../../Header/Header";
-import Button from "../../Button/Button";
-import Input from "../../Input/Input";
+// import Button from "../../Button/Button";
+// import Input from "../../Input/Input";
 
-import CartItem from "../../CartItem/CartItem";
-import { CartItemInterface } from "../../CartItem/CartItem.interface";
+// import CartItem from "../../CartItem/CartItem";
+// // import { CartItemInterface } from "../../CartItem/CartItem.interface";
 
-import { StoreType } from "../../../store/store";
-import { useSelector } from "react-redux";
+// import { StoreType } from "../../../store/store";
+// import { useSelector } from "react-redux";
 
 export default function Cart() {
-    const {products} = useSelector((state: StoreType) => state.cart);
+    // const {products} = useSelector((state: StoreType) => state.cart);
 
     return (
         <div>
             <Header>Корзина</Header>
-            {products.map((item) => (
+            {/* {products.map((item) => (
                 <CartItem 
                     key={item.id}
                     image={item.image}
@@ -22,7 +22,7 @@ export default function Cart() {
                     price={item.price}
                     count={item.count}
                 />
-            ))}
+            ))} */}
         </div>
     );
 }
